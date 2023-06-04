a7c759799c7b09530b5086dc9e28e0b29f4395be
prantae <neelratan@live.com>
2023-06-04 14:15:50 +0530

    Initial Commit

---
+++ Mvvm-iOS/Mvvm_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mvvm_iOSApp.swift
+//  Mvvm-iOS
+//
+//  Created by PRANTAE-WS-07  on 04/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Mvvm_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
