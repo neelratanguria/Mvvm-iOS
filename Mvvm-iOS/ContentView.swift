5c8fc184e9e706b316a4c3cce7ab4bb9d3bce47b
prantae <neelratan@live.com>
2023-06-04 15:51:04 +0530

    Update ContentView.swift

--- Mvvm-iOS/ContentView.swift
+++ Mvvm-iOS/ContentView.swift
@@ -8,14 +8,82 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let columns : [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+    ]
+    
+    @State var moves: [Move?] = Array(repeating: nil, count: 9)
+    @State var isHumanTurn = true
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        GeometryReader { geometry in
+            VStack{
+                Spacer()
+                LazyVGrid(columns: columns){
+                    ForEach(0..<9) { i in
+                        ZStack {
+                            Circle()
+                                .frame(
+                                    width: geometry.size.width/3 - 15,
+                                    height: geometry.size.width/3
+                                )
+                            
+                            if(moves[i]==nil) {
+                                
+                            } else{
+                                if(moves[i]?.player == .Human){
+                                    Image(systemName: "xmark")
+                                        .resizable()
+                                        .frame(
+                                            width: 40,
+                                            height: 40
+                                        )
+                                        .foregroundColor(.gray)
+                                } else {
+                                    Image(systemName: "circle")
+                                        .resizable()
+                                        .frame(
+                                            width: 40,
+                                            height: 40
+                                        )
+                                        .foregroundColor(.gray)
+                                }
+                                
+                            }
+                        }
+                        .onTapGesture {
+                            moves[i] = Move(player: isHumanTurn ? .Human : .Computer, boardIndex: i)
+                            isHumanTurn.toggle()
+                            
+                        }
+                    }
+                }
+                Spacer()
+                Button(action: {
+                    moves = Array(repeating: nil, count: 9)
+                }, label: {
+                    Text("Reset")
+                })
+            }
+            
         }
-        .padding()
+    }
+}
+
+enum Player {
+    case Human, Computer
+}
+
+struct Move {
+    let player: Player
+    let boardIndex: Int
+    
+    var indicator: String {
+        return player == .Human ? "xmar" : "circle"
     }
 }
 
